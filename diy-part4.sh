658f2d195ee03ebf78e3188444f41784df460ed8
小渔 <864529498@qq.com>
2023-01-13 23:30:04 +0800

    精简脚本

--- diy-part4.sh
+++ diy-part4.sh
@@ -13,10 +13,5 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-#Add a feed source
-#echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-#echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # 预下载主题
 git clone -b master https://github.com/yuos-bit/luci-theme-netgear package/molun/luci-theme-netgear
