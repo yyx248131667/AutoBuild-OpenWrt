8d259a8b4635e8df88beb28577ea69b0de9772b3
小渔 <864529498@qq.com>
2023-02-01 11:42:35 +0800

    修改软件版本

--- diy-part6.sh
+++ diy-part6.sh
@@ -12,7 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+# 修改软件包版本为lienol21.02
+sed -i 's/https://git.openwrt.org/feed/packages.git;openwrt-21.02/https://github.com/Lienol/openwrt-packages.git;21.02/g' feeds.conf.default
+sed -i 's/https://git.openwrt.org/project/luci.git;openwrt-21.02/https://github.com/Lienol/openwrt-luci.git;21.02/g' feeds.conf.default
 # 增加软件包
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
@@ -25,3 +27,5 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' fee
 
 # 修改默认dnsmasq为dnsmasq-full
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+
+# 单独拉取 default-settings
