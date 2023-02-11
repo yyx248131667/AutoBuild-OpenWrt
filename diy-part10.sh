483b077b15abad28cfc7c24508be0a1603e10063
小渔 <864529498@qq.com>
2023-02-11 09:09:15 +0800

    Update diy-part10.sh

--- diy-part10.sh
+++ diy-part10.sh
@@ -50,7 +50,7 @@ sed -i 's/odhcpd-ipv6only/ipset ip-full default-settings luci/g' include/target.
 sed -i 's/kmod-mt7603 kmod-mt7615e kmod-mt7615-firmware/kmod-mt7603e kmod-mt7615d luci-app-mtwifi -wpad-openssl/g' target/linux/ramips/image/mt7621.mk
 
 # 单独拉取 default-settings
-git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
+git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/yuos
 # git clone -b lede-default-settings https://github.com/yuos-bit/other package/default-settings
 
 # 单独拉取 lean包到package 目录
@@ -60,6 +60,6 @@ git clone -b main https://github.com/yuos-bit/other package/lean
 rm -rf package/lean/default-settings/
 
 # 设置闭源驱动开机自启
-wget -O package/default-settings/S99wifiopen https://github.com/yuos-bit/other/releases/download/shell/wifi.up.sh
+wget -O package/yuos/default-settings/S99wifiopen https://github.com/yuos-bit/other/releases/download/shell/wifi.up.sh
 # 自启权限
-chmod 777 package/default-settings/S99wifiopen
+chmod 777 package/yuos/default-settings/S99wifiopen
