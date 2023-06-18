4a64027a142e1b22e57f7ff20032ad98a7d50abe
小渔 <864529498@qq.com>
2023-06-18 19:41:56 +0800

    更新补丁

--- diy-part8B.sh
+++ diy-part8B.sh
@@ -61,75 +61,23 @@ wget https://github.com/quintus-lab/openwrt-rockchip/raw/master/patches/910-mini
 wget https://github.com/quintus-lab/openwrt-rockchip/raw/master/patches/911-dnsmasq-filter-aaaa.patch
 wget https://github.com/LGA1150/openwrt-fullconenat/raw/master/patches/000-printk.patch
 
-patch -p1 < ./tools-add-upx-ucl-support.patch
-patch -p1 < ./dnsmasq_add_filter_aaaa_option.patch
-patch -p1 < ./fw3_fullconenat.patch
-patch -p1 < ./luci-app-firewall_add_fullcone.patch
-patch -p1 < ./add-5.14-support.patch
-patch -p1 < ./mod-for-k514.patch
-patch -p1 < ./mini-ttl.patch
-patch -p1 < ./dnsmasq-filter-aaaa.patch
-patch -p1 < ./printk.patch
+patch -p1 < ./0001-tools-add-upx-ucl-support.patch
+patch -p1 < ./1001-dnsmasq_add_filter_aaaa_option.patch
+patch -p1 < ./1002-fw3_fullconenat.patch
+patch -p1 < ./1003-luci-app-firewall_add_fullcone.patch
+patch -p1 < ./2001-add-5.14-support.patch
+patch -p1 < ./2003-mod-for-k514.patch
+patch -p1 < ./910-mini-ttl.patch
+patch -p1 < ./911-dnsmasq-filter-aaaa.patch
+patch -p1 < ./000-printk.patch
 
 #shortcut-fe patches
 wget https://github.com/MeIsReallyBa/Openwrt-sfe-flowoffload-linux-5.4/raw/master/952-net-conntrack-events-support-multiple-registrant.patch
 wget https://github.com/MeIsReallyBa/Openwrt-sfe-flowoffload-linux-5.4/raw/master/999-shortcut-fe-support.patch
 
-patch -p1 < ./net-conntrack-events-support-multiple-registrant.patch
-patch -p1 < ./shortcut-fe-support.patch
+patch -p1 < ./952-net-conntrack-events-support-multiple-registrant.patch
+patch -p1 < ./999-shortcut-fe-support.patch
 
 
 #FullCone Patch
 git clone -b master --single-branch https://github.com/lxz1104/openwrt-fullconenat package/fullconenat
-
-# Patch FireWall for fullcone
-mkdir package/network/config/firewall/patches
-wget -P package/network/config/firewall/patches/ https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/fullconenat.patch
-
-pushd feeds/luci
-wget -O- https://github.com/LGA1150/fullconenat-fw3-patch/raw/master/luci.patch | git apply
-popd
-
-#Patch Kernel for fullcone
-pushd target/linux/generic/hack-5.4
-wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/hack-5.4/952-net-conntrack-events-support-multiple-registrant.patch
-popd
-
-# SFE kernel patch
-pushd target/linux/generic/hack-5.4
-wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/hack-5.4/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
-popd
-svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/shortcut-fe package/new/shortcut-fe
-svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/shortcut-fe/fast-classifier package/new/fast-classifier
-
-#AutoCore
-svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/autocore package/lean/autocore
-#coremark
-rm -rf ./feeds/packages/utils/coremark
-rm -rf ./package/feeds/packages/coremark
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/coremark package/lean/coremark
-sed -i 's,-DMULTIT,-Ofast -DMULTIT,g' package/lean/coremark/Makefile
-
-#ssrp
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
-rm -rf ./feeds/packages/net/kcptun
-rm -rf ./feeds/packages/net/shadowsocks-libev
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev package/lean/shadowsocksr-libev
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt package/lean/pdnsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package/lean/v2ray
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/kcptun package/lean/kcptun
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/lean/v2ray-plugin
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/srelay package/lean/srelay
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/lean/microsocks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks package/lean/dns2socks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/lean/redsocks2
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/proxychains-ng package/lean/proxychains-ng
-git clone -b master --single-branch https://github.com/pexcn/openwrt-ipt2socks package/lean/ipt2socks
-git clone -b master --single-branch https://github.com/aa65535/openwrt-simple-obfs package/lean/simple-obfs
-svn co https://github.com/coolsnowwolf/packages/trunk/net/shadowsocks-libev package/lean/shadowsocks-libev
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan package/lean/trojan
-svn co https://github.com/project-openwrt/openwrt/trunk/package/lean/tcpping package/lean/tcpping
-
-#install upx
-mkdir -p staging_dir/host/bin/
-ln -s /usr/bin/upx-ucl staging_dir/host/bin/upx
