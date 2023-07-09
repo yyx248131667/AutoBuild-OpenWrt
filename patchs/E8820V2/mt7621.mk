1312b97a5f0e3d4ba42156abc7cb44d454127215
小渔 <864529498@qq.com>
2023-07-09 16:30:27 +0800

    更新

--- patchs/E8820V2/mt7621.mk
+++ patchs/E8820V2/mt7621.mk
@@ -1684,7 +1684,7 @@ define Device/zte_e8820v2
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := ZTE
   DEVICE_MODEL := E8820V2
-  DEVICE_PACKAGES := \
-    kmod-mt7603 kmod-mt76x2 kmod-usb3 kmod-usb-ledtrig-usbport luci
+  DEVICE_PACKAGES := kmod-mt7603e kmod-mt7612e luci-app-mtwifi kmod-usb3 \
+    kmod-usb-ledtrig-usbport luci
 endef
 TARGET_DEVICES += zte_e8820v2
