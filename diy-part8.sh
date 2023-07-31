1edfe2373a91f5a8cc5f9720e7276e47f19ad637
小渔 <864529498@qq.com>
2023-07-31 11:03:50 +0800

    Update diy-part8.sh

--- diy-part8.sh
+++ diy-part8.sh
@@ -65,8 +65,8 @@ git clone -b main --single-branch https://github.com/yuos-bit/other package/yuos
 # wget https://raw.githubusercontent.com/gl-inet/gl-infra-builder/main/patches-mt798x-7.6.6.1/2006-Kernel-support-software-acceleration.patch
 # patch -p1 < ./2006-Kernel-support-software-acceleration.patch
 # SFE
-wget https://raw.githubusercontent.com/yuos-bit/Openwrt-sfe-flowoffload-linux-5.4/master/952-net-conntrack-events-support-multiple-registrant.patch
-wget https://raw.githubusercontent.com/yuos-bit/Openwrt-sfe-flowoffload-linux-5.4/master/999-shortcut-fe-support.patch
+# wget https://raw.githubusercontent.com/yuos-bit/Openwrt-sfe-flowoffload-linux-5.4/master/952-net-conntrack-events-support-multiple-registrant.patch
+# wget https://raw.githubusercontent.com/yuos-bit/Openwrt-sfe-flowoffload-linux-5.4/master/999-shortcut-fe-support.patch
 
-cp -R ./952-net-conntrack-events-support-multiple-registrant.patch $GITHUB_WORKSPACE/openwrt/target/linux/ramips/patches-5.4/952-net-conntrack-events-support-multiple-registrant.patch
-cp -R ./999-shortcut-fe-support.patch $GITHUB_WORKSPACE/openwrt/target/linux/ramips/patches-5.4/999-shortcut-fe-support.patch
+#cp -R ./952-net-conntrack-events-support-multiple-registrant.patch $GITHUB_WORKSPACE/openwrt/target/linux/ramips/patches-5.4/952-net-conntrack-events-support-multiple-registrant.patch
+#cp -R ./999-shortcut-fe-support.patch $GITHUB_WORKSPACE/openwrt/target/linux/ramips/patches-5.4/999-shortcut-fe-support.patch
