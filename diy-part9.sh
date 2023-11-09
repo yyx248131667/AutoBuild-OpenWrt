d28cd416d950e6e5a4af74d49f66f09fea490024
小渔 <864529498@qq.com>
2023-11-09 22:16:02 +0800

    修正脚本

--- diy-part9.sh
+++ diy-part9.sh
@@ -40,9 +40,9 @@ sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.defa
 
 # 添加5.4内核ACC、shortcut-fe补丁
 # openwrt21.02 netfilter补丁\
-cp -rf $GITHUB_WORKSPACE/patchs/firewall/* /package/firewall
+cp -rf $GITHUB_WORKSPACE/patchs/firewall/* package/firmware/
 
-patch -p1 < package/firewall/001-fix-firewall-flock.patch
-patch -p1 < package/firewall/322-mt7621-fix-cpu-clk-add-clkdev.patch
-patch -p1 < ppackage/firewall/613-netfilter_optional_tcp_window_check.patch
-patch -p1 < package/firewall/luci-app-firewall_add_fullcone.patch
+patch -p1 < package/firmware/001-fix-firewall-flock.patch
+patch -p1 < package/firmware/322-mt7621-fix-cpu-clk-add-clkdev.patch
+patch -p1 < ppackage/firmware/613-netfilter_optional_tcp_window_check.patch
+patch -p1 < package/firmware/luci-app-firewall_add_fullcone.patch
