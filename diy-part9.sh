70a49aea7201d211fdecd43a61e8c1163e927d8a
小渔 <864529498@qq.com>
2023-11-09 20:14:46 +0800

    更新

--- diy-part9.sh
+++ diy-part9.sh
@@ -40,8 +40,7 @@ sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.defa
 
 # 添加5.4内核ACC、shortcut-fe补丁
 # openwrt21.02 netfilter补丁\
-mkdir $GITHUB_WORKSPACE/openwrt/patchs/firewall
-cp -R $GITHUB_WORKSPACE/patchs/firewall $GITHUB_WORKSPACE/openwrt/patchs/firewall
+cp -rf $GITHUB_WORKSPACE/patchs/firewall openwrt/patchs/firewall
 
 patch -p1 < patchs/firewall/001-fix-firewall-flock.patch
 patch -p1 < patchs/firewall/322-mt7621-fix-cpu-clk-add-clkdev.patch
