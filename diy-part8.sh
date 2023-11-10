a3101914b0a408c843902369241d62d1ce0d8cf7
小渔 <864529498@qq.com>
2023-11-10 21:06:28 +0800

    降级fullconenat-nft

--- diy-part8.sh
+++ diy-part8.sh
@@ -82,5 +82,3 @@ cp -rf $GITHUB_WORKSPACE/openwrt/package/yuos/upx $GITHUB_WORKSPACE/openwrt/tool
 # nft-fullcone
 git clone -b main --single-branch https://github.com/fullcone-nat-nftables/nftables-1.0.5-with-fullcone package/nftables
 git clone -b master --single-branch https://github.com/fullcone-nat-nftables/libnftnl-1.2.4-with-fullcone package/libnftnl
-
-# 
