3c84e9ffd04c90e7341e1c3f786ebebd450e81b7
小渔 <864529498@qq.com>
2023-11-19 20:59:37 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -20,10 +20,10 @@ sed -i 's/git.openwrt.org\/feed\/packages.git;openwrt-19.07/github.com\/Lienol\/
 sed -i 's/git.openwrt.org\/project\/luci.git;openwrt-19.07/github.com\/coolsnowwolf\/luci.git;master/g' feeds.conf.default
 
 # 增加软件包
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;main' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages.git;master' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small.git;master' feeds.conf.default
+# sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' feeds.conf.default
 
 
 # 预下载主题
