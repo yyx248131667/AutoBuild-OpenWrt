443504f40e08118e789591ff8b9c0afb937b8306
小渔 <864529498@qq.com>
2024-01-07 22:18:34 +0800

    修改脚本

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,9 +20,6 @@ sed -i '$a src-git small8 https://github.com/kenzok8/small-package.git;main' fee
 # 修改默认编译LUCI进系统
 sed -i 's/ppp-mod-pppoe/iptables-mod-tproxy iptables-mod-extra ipset ip-full ppp-mod-pppoe curl ca-certificates/g' include/target.mk
 
-# 单独拉取 default-settings
-git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/yuos
-
 # 拉取主题
 git clone -b theme https://github.com/yuos-bit/other package/theme
 # 设置闭源驱动开机自启
@@ -31,10 +28,6 @@ sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl add
 # 单独拉取软件包
 git clone -b Lienol-default-settings https://github.com/yuos-bit/other package/default-settings
 git clone -b main --single-branch https://github.com/yuos-bit/other package/yuos
-
-# 修改/tools/Makefile
-sed -i '11a tools-y += ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
-cp -rf $GITHUB_WORKSPACE/patchs/5.4/package/* tools/
 
 # 更改默认wifi
 cp -rf $GITHUB_WORKSPACE/patchs/NX30Pro/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
