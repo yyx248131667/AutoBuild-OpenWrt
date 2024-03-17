34a32961046f20fe9f8487a503268e9e2f351d70
小渔 <864529498@qq.com>
2024-03-17 21:17:56 +0800

    Update diy-part10.sh

--- diy-part10.sh
+++ diy-part10.sh
@@ -59,3 +59,13 @@ git clone -b main https://github.com/yuos-bit/other package/lean
 
 # 设置闭源驱动开机自启
 sed -i '2a ifconfig rai0 up\nifconfig ra0 up\nbrctl addif br-lan rai0\nbrctl addif br-lan ra0' package/base-files/files/etc/rc.local
+# 测试编译时间
+YUOS_DATE="$(date +%Y.%m.%d)(自用测试版)"
+BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
+echo "Write build date in openwrt : $BUILD_DATE"
+echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
+sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
+sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
