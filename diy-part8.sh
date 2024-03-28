8b578a7b894aa51546e10575c765c93435ffa5ef
小渔 <864529498@qq.com>
2024-03-28 12:20:29 +0800

    Update diy-part8.sh

--- diy-part8.sh
+++ diy-part8.sh
@@ -135,7 +135,7 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/fast-classifier p
 mkdir -p staging_dir/host/bin/
 ln -s /usr/bin/upx-ucl staging_dir/host/bin/upx
 
-#cmake patches
-patch -p1 < $GITHUB_WORKSPACE/openwrt/tools/cmake/patches/100-no-testing.patch
-patch -p1 < $GITHUB_WORKSPACE/openwrt/tools/cmake/patches/120-curl-fix-libressl-linking.patch
-patch -p1 < $GITHUB_WORKSPACE/openwrt/tools/cmake/patches/130-bootstrap_parallel_make_flag.patch
+# #cmake patches
+# patch -p1 < $GITHUB_WORKSPACE/openwrt/tools/cmake/patches/100-no-testing.patch
+# patch -p1 < $GITHUB_WORKSPACE/openwrt/tools/cmake/patches/120-curl-fix-libressl-linking.patch
+# patch -p1 < $GITHUB_WORKSPACE/openwrt/tools/cmake/patches/130-bootstrap_parallel_make_flag.patch
