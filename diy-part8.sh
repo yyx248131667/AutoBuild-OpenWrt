be446c7072ed26bd0a05b1df74ef0c1de631e140
小渔 <864529498@qq.com>
2024-03-29 01:06:54 +0800

    Update diy-part8.sh

--- diy-part8.sh
+++ diy-part8.sh
@@ -79,7 +79,7 @@ git clone -b main --single-branch https://github.com/siwind/luci-app-usb_printer
 
 
 # 添加gn
-cp -rfT $GITHUB_WORKSPACE/patchs/5.4/gn/ $GITHUB_WORKSPACE/openwrt/package/gn/
+# cp -rfT $GITHUB_WORKSPACE/patchs/5.4/gn/ $GITHUB_WORKSPACE/openwrt/package/gn/
 
 # 添加iptables-mod-socket
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/iptables-mod-socket.patch $GITHUB_WORKSPACE/openwrt/package/iptables-mod-socket.patch
