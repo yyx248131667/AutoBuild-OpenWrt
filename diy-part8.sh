d60bdf1c2a813824bbaca62a5b06c61d93ff009d
小渔 <864529498@qq.com>
2024-04-01 22:51:48 +0800

    add sfe补丁

--- diy-part8.sh
+++ diy-part8.sh
@@ -111,4 +111,5 @@ popd
 
 # SFE kernel patch
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/hack-5.4/* target/linux/generic/hack-5.4/
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/pending-5.4/* target/linux/generic/pending-5.4/
 cp -rf $GITHUB_WORKSPACE/patchs/5.4/sfe/* package/yuos/
