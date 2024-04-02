03c28331c6f0842014f76b9919d3df64cfdbb89b
小渔 <864529498@qq.com>
2024-04-02 23:27:11 +0800

    Update diy-partA.sh

--- diy-partA.sh
+++ diy-partA.sh
@@ -121,3 +121,8 @@ native Linux tools such as ss.\n\
 endef\n\
 \n\
 $(eval $(call KernelPackage,inet-diag))' package/kernel/linux/modules/netsupport.mk
+
+# 升级golang
+find . -type d -name "golang" -exec rm -r {} +
+mkdir -p feeds/packages/lang/golang/
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/golang/* feeds/packages/lang/golang/
