71f2f4af31bfcc6b72a860c4d39b5360d8afb832
小渔 <864529498@qq.com>
2024-04-11 16:19:19 +0800

    # 添加 kmod-nf-socket 依赖

--- diy-part8.sh
+++ diy-part8.sh
@@ -143,6 +143,53 @@ endef\n\
 \n\
 $(eval $(call KernelPackage,inet-diag))' package/kernel/linux/modules/netsupport.mk
 
+# 添加 kmod-nf-socket 依赖
+sed -i '/$(eval \$(call KernelPackage,nft-queue))/a\
+\
+define KernelPackage/nft-socket\n\
+  SUBMENU:=$(NF_MENU)\n\
+  TITLE:=Netfilter nf_tables socket support\n\
+  DEPENDS:=+kmod-nft-core +kmod-nf-socket\n\
+  FILES:=$(foreach mod,$(NFT_SOCKET-m),$(LINUX_DIR)/net/$(mod).ko)\n\
+  AUTOLOAD:=$(call AutoProbe,$(notdir $(NFT_SOCKET-m)))\n\
+  KCONFIG:=$(KCONFIG_NFT_SOCKET)\n\
+endef\n\
+\n\
+$(eval $(call KernelPackage,nft-socket))\n\
+\n\
+define KernelPackage/nft-tproxy\n\
+  SUBMENU:=$(NF_MENU)\n\
+  TITLE:=Netfilter nf_tables tproxy support\n\
+  DEPENDS:=+kmod-nft-core +kmod-nf-tproxy +kmod-nf-conntrack\n\
+  FILES:=$(foreach mod,$(NFT_TPROXY-m),$(LINUX_DIR)/net/$(mod).ko)\n\
+  AUTOLOAD:=$(call AutoProbe,$(notdir $(NFT_TPROXY-m)))\n\
+  KCONFIG:=$(KCONFIG_NFT_TPROXY)\n\
+endef\n\
+\n\
+$(eval $(call KernelPackage,nft-tproxy))\n\
+\n\
+define KernelPackage/nft-compat\n\
+  SUBMENU:=$(NF_MENU)\n\
+  TITLE:=Netfilter nf_tables compat support\n\
+  DEPENDS:=+kmod-nft-core +kmod-nf-ipt\n\
+  FILES:=$(foreach mod,$(NFT_COMPAT-m),$(LINUX_DIR)/net/$(mod).ko)\n\
+  AUTOLOAD:=$(call AutoProbe,$(notdir $(NFT_COMPAT-m)))\n\
+  KCONFIG:=$(KCONFIG_NFT_COMPAT)\n\
+endef\n\
+\n\
+$(eval $(call KernelPackage,nft-compat))\n\
+\n\
+define KernelPackage/nft-xfrm\n\
+  SUBMENU:=$(NF_MENU)\n\
+  TITLE:=Netfilter nf_tables xfrm support (ipsec)\n\
+  DEPENDS:=+kmod-nft-core\n\
+  FILES:=$(foreach mod,$(NFT_XFRM-m),$(LINUX_DIR)/net/$(mod).ko)\n\
+  AUTOLOAD:=$(call AutoProbe,$(notdir $(NFT_XFRM-m)))\n\
+  KCONFIG:=$(KCONFIG_NFT_XFRM)\n\
+endef\n\
+\n\
+$(eval $(call KernelPackage,nft-xfrm))' package/kernel/linux/modules/netfilter.mk
+
 
 ### 后补的
 
