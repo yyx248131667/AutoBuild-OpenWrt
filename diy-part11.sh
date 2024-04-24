7f1b8f2a0cd7b39fa9bb6defe19bc7cad28ec674
star <32487675+yyx248131667@users.noreply.github.com>
2024-04-24 22:14:00 +0800

    Update diy-part11.sh

--- diy-part11.sh
+++ diy-part11.sh
@@ -11,18 +11,18 @@
 #=================================================
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 修改网关
-sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名称
-sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
 # sed -i "s/OpenWrt/小渔学长 build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/default-settings/files/zzz-default-settings
 
 # 修改默认wifi名称ssid为Xiaomi-Wifi
-sed -i 's/ssid=OpenWrt/ssid=Xiaoyu-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/ssid=OpenWrt/ssid=Xiaoyu-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 
 # 修改登陆密码
@@ -40,7 +40,7 @@ sed -i 's/ssid=OpenWrt/ssid=Xiaoyu-Wifi/g' package/kernel/mac80211/files/lib/wif
     sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
 
 # 修改 edge 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-#sed -i 's/luci-theme-bootstrap/luci-theme-edge/g' openwrt/feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-design/g' openwrt/feeds/luci/collections/luci/Makefile
 
 #Enable 802.11k/v/r
 #sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
